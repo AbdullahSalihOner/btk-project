8ed629631948330ef9018b9bb5257bf57413e277
Abdullah Salih Öner <riyadlioner00@gmail.com>
2023-11-09 19:24:47 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'btk-project';
-  variable = 'degisken';
+export class AppComponent implements OnInit{
+ 
+
+  degisken1: number = 5;
+  degisken2: number = 7;
+  degisken3: number;
+
+  constructor(){
+    this.degisken3 = this.topla();
+    console.log(this.degisken3);
+  }
+
+  ngOnInit(){
+    console.log("Started")
+  }
+
+  getTotal(number1:number, number2:number) : number{
+    let total:number = number1 + number2;
+    return total;
+    
+  }
+
+  topla():number{
+    let toplam:number;
+    toplam = this.degisken1 + this.degisken2;
+    return toplam;
+  }
 }
